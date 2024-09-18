a782d8852ae514c872d772121c437b61232e3137
Lorna <lornnaa@live.com>
2024-09-18 01:43:26 +0100

    Initial commit

---
+++ InputParser/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("### Warehouse Counter ###");
+Console.WriteLine("What is your stock?");
+
+var stock = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(stock))
+{
+    Console.WriteLine("Please enter a valid stock");
+    return;
+}
+
+var result = "";
+var letterCounts = new Dictionary<char, int>();
+
+var isOlderVersionMatch = Regex.IsMatch(stock, @"^[a-zA-Z ]+$");
+var isNewerVersionMatch = Regex.IsMatch(stock, @"^#p2#(?: \d+[a-zA-Z])+$");
+
+if (!isOlderVersionMatch && !isNewerVersionMatch)
+{
+    Console.WriteLine("Invalid stock format");
+    return;
+}
+
+if (isOlderVersionMatch)
+{
+    var letters = stock.Replace(" ", "").Where(c => char.IsLetter(c));
+
+    foreach (var c in letters)
+    {
+        letterCounts[c] = letterCounts.TryGetValue(c, out int value) ? ++value : 1;
+    }
+}
+else if (isNewerVersionMatch)
+{
+    var parts = stock.Split(' ').Skip(1);
+
+    foreach (var part in parts)
+    {
+        var letter = part.Last();
+        var count = int.Parse(part[..^1]);
+
+        if (letterCounts.ContainsKey(letter))
+        {
+            letterCounts[letter] += count;
+        }
+        else
+        {
+            letterCounts[letter] = count;
+        }
+    }
+}
+
+result = string.Join(" ", letterCounts.OrderBy(kvp => kvp.Key).Select(kvp => $"{kvp.Value}{kvp.Key}"));
+
+Console.WriteLine($"Output: {result}");
+Console.ReadLine();
---
+++ README.md
@@ -0,0 +1,13 @@
+# Input Parser
+
+A simple console application used for counting items in a warehouse.
+
+## Examples
+
+Input: `aaaa eeeeee bb c aa ccc d`
+Output: `6a 2b 4c 1d 6e`
+
+Input: `#p2# 4a 4b 7c 2a 5d 2a 6d 1e 2d`
+Output: `8a 4b 7c 13d 1e`
+
+*Note I've corrected the output of the 2nd example (as provided in the tech test doc) with difference being `8a` instead of `9a`.
